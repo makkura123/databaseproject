d6741251f998b9fdf8fd4df220a9b6e6fcb239fe
Matthew Draper <draper_matthewjohn@yahoo.com>
2016-04-12 11:50:06 -0700

    Player Flags

--- Assets/_Scripts/PlayerControl.cs
+++ Assets/_Scripts/PlayerControl.cs
@@ -22,7 +22,7 @@ public class PlayerControl : MonoBehaviour{
 	Text
 		Role;
 	[SerializeField]
-	Text
+	Image
 		Residence;
 	[SerializeField]
 	Text
@@ -36,9 +36,10 @@ public class PlayerControl : MonoBehaviour{
 	[SerializeField]
 	Text
 		SummonerName;
+
 	[SerializeField]
-	Text
-		Team;
+	Sprite[]
+		ResidenceSprites;
 
 	const string _filename = "LeagueDB.s3db";
 	private IDbConnection _dbcon;
@@ -108,7 +109,7 @@ public class PlayerControl : MonoBehaviour{
 		for (int i = 0; _dbr.Read(); i++) {
 			SummonerName.text = _dbr.GetString (0);
 			Role.text = _dbr.GetString (1);
-			Residence.text = _dbr.GetString (2);
+			Residence.sprite = ResidenceSprite(_dbr.GetString (2));
 			Header.text = _dbr.GetString (3) + " " + _dbr.GetString (4);
 			KDA.text = _dbr.GetString (5) + "/" + _dbr.GetString (6) + "/" + _dbr.GetString (7);
 			CS.text = _dbr.GetString (8);
@@ -117,6 +118,46 @@ public class PlayerControl : MonoBehaviour{
 			LastChamp.text = champName;
 		}
 		CloseConnection ();
+	}
+
+	Sprite ResidenceSprite(string resName)
+	{
+		if (resName == "Brazil")
+			return ResidenceSprites [0];
+		else if (resName == "Canada")
+			return ResidenceSprites [1];
+		else if (resName == "China")
+			return ResidenceSprites [2];
+		else if (resName == "Denmark")
+			return ResidenceSprites [3];
+		else if (resName == "France")
+			return ResidenceSprites [4];
+		else if (resName == "Germany")
+			return ResidenceSprites [5];
+		else if (resName == "Hong Kong")
+			return ResidenceSprites [6];
+		else if (resName == "South Korea")
+			return ResidenceSprites [7];
+		else if (resName == "Netherlands")
+			return ResidenceSprites [8];
+		else if (resName == "Philippines")
+			return ResidenceSprites [9];
+		else if (resName == "Romania")
+			return ResidenceSprites [10];
+		else if (resName == "Spain")
+			return ResidenceSprites [11];
+		else if (resName == "Sweden")
+			return ResidenceSprites [12];
+		else if (resName == "Taiwan")
+			return ResidenceSprites [13];
+		else if (resName == "Thailand")
+			return ResidenceSprites [14];
+		else if (resName == "United Kingdom")
+			return ResidenceSprites [15];
+		else if (resName == "U.S.A.")
+			return ResidenceSprites [16];
+		else
+			throw new InvalidOperationException ("Unrecognised residenceName : " + resName);
 	}
 
 	// Button function for clicking on a champion, loads the correct scene and saves the last played champion to use it in a query in that scene
